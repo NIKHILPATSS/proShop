250df7c700e3bfaaded83092ca6e9faee6ec6b73
Nikhil <patangenikhil08@gmail.com>
2021-07-20 23:51:06 +0530

    created 3 user routes for login,register and get profile . added cart reducer in FE along with storing in localStorageand also creatd authMiddleware

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,14 +1,14 @@
 import mongoose from 'mongoose';
-
+import bcrypt from 'bcryptjs'
 
 const UserSchema = mongoose.Schema({
     name: {
         type: String,
-        require: true
+        required: true
     },
     email: {
         type: String,
-        require: true,
+        required: true,
         unique: true,
         match: [
             /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
@@ -17,18 +17,30 @@ const UserSchema = mongoose.Schema({
     },
     password: {
         type: String,
-        require: true,
+        required: true,
         minlength: 6
     },
     isAdmin: {
         type: Boolean,
-        require: true,
+        required: true,
         default: false
     }
 }, {
     timestamps: true
 })
 
+
+UserSchema.methods.matchPassword = async function(enteredPassword){
+    return await bcrypt.compare(enteredPassword,this.password)
+}
+
+UserSchema.pre('save',async function(next){
+    if(!this.isModified('password')){
+        next();
+    }
+    const salt =  await bcrypt.genSalt(10)
+    this.password = await bcrypt.hash(this.password,salt)
+})
 
 const User = mongoose.model('User', UserSchema);
 
--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,28 +1,35 @@
 import React, { useState, useEffect } from 'react';
-import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap';
+import { Row, Col, Image, ListGroup, Card, Button ,Form } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import { useDispatch,useSelector } from 'react-redux';
+
 import Rating from '../components/Rating';
-import axios from 'axios';
+import { listProductDetails } from '../actions/productAction';
+import Loader from '../components/Loader';
+import Message from '../components/Message';
 
-const ProductScreen = ({ match }) =>
+const ProductScreen = ({ match , history }) =>
 {
-    const [product, setProduct] = useState({});
+    const [qty,setQty] = useState(1);
 
+    const dispatch = useDispatch();
+    const productDetails = useSelector(state => state.productDetails);
+    const {loading,error,product} = productDetails;
     useEffect(() =>
     {
-        const fetchProduct = async () =>
-        {
-            const { data } = await axios.get(`/api/products/${ match.params.id }`);
-            setProduct(data);
-        }
-        fetchProduct()
-    }, [match])
+        dispatch(listProductDetails(match.params.id))
+    }, [dispatch,match])
+
+    const addToCartHandler = () => {
+        history.push(`/cart/${match.params.id}?qty=${qty}`)
+    }
 
     return (
         <>
             <Link className='btn btn-light my-3' to='/'>
                 Go Back
             </Link>
+            {loading ? <Loader /> : error ? <Message variant='danger'>{error}</Message> : (
             <Row>
                 <Col md={6} >
                     <Image src={product.image} alt={product.name} fluid />
@@ -66,8 +73,24 @@ const ProductScreen = ({ match }) =>
                                     </Col>
                                 </Row>
                             </ListGroup.Item>
+                             {product.countInStock > 0 && (
+                                 <ListGroup.Item>
+                                    <Row>
+                                        <Col>Qty</Col>
+                                        <Col>
+                                            <Form.Control as='select' value={qty} onChange={(e) =>setQty(e.target.value)} >
+                                                {[...Array(product.countInStock).keys()].map( (x) => (
+                                                    <option key={x + 1} value={x + 1}>{x + 1}</option>
+                                                ))}
+                                            </Form.Control>
+                                        </Col>
+                                    </Row>
+                                 </ListGroup.Item>
+                             )}
                             <ListGroup.Item>
-                                <Button className='btn-block' type='button' disabled={product.countInStock === 0}>
+                                <Button className='btn-block' type='button' disabled={product.countInStock === 0}
+                                        onClick={addToCartHandler}
+                                >
                                     ADD TO CART
                                 </Button>
                             </ListGroup.Item>
@@ -75,6 +98,7 @@ const ProductScreen = ({ match }) =>
                     </Card>
                 </Col>
             </Row>
+            )}
         </>
     );
 };
