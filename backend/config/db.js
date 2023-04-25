9b6c67e537c37448070c35372112876a45c5acc8
Nikhil Patange <patangenikhil08@gmail.com>
2023-04-25 18:29:13 +0530

    added comment

--- backend/config/db.js
+++ backend/config/db.js
@@ -4,6 +4,7 @@ const connectDB = async () =>
 {
     try
     {
+        console.log("process.env.MONGO_URI",process.env.MONGO_URI)
         const conn = await mongoose.connect(process.env.MONGO_URI, {
             useNewUrlParser: true,
             useCreateIndex: true,
