d942caad9324fd1d3dc9c691d3d80dba88893edf
Nikhil Patange <patangenikhil08@gmail.com>
2023-04-25 19:08:18 +0530

    removed comment

--- backend/config/db.js
+++ backend/config/db.js
@@ -4,14 +4,12 @@ const connectDB = async () =>
 {
     try
     {
-        console.log("process.env.MONGO_URI",process.env.MONGO_URI)
         const conn = await mongoose.connect(process.env.MONGO_URI, {
             useNewUrlParser: true,
             useCreateIndex: true,
             useFindAndModify: false,
             useUnifiedTopology: true
         });
-        console.log("conn",conn)
         console.log(`Mongo DB Connected : ${ conn.connection.host }`.cyan.underline.bold);
     } catch (error)
     {
--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -9,7 +9,6 @@ const protect = asyncHandler(async (req, res, next) => {
 		req.headers.authorization.startsWith('Bearer')
 	) {
 		try {
-			console.log("Here")
 			token = req.headers.authorization.split(' ')[1];
 			const decoded = jwt.verify(token, process.env.JWT_SECRET);
 			req.user = await User.findById(decoded.id).select('-password');
